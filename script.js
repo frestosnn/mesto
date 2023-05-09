92a5db597c28e66ffad0fa0a076da4325a452541
Комарова Анастасия <frestos.n@yandex.ru>
2023-05-09 23:09:37 +0300

    реализовать закрытие и открытие попапа

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let popup = document.querySelector('.popup');
+let editBotton = document.querySelector('.profile__edit-button');
+let closeBotton = document.querySelector('.popup__button-close');
 
 function openPopup() {
-  if (popup.length === 0) {
-    popup.classList.add('popup_opened');
-  } else {
-    popup.classList.remove('popup_opened');
-  }
+  popup.classList.add('popup__opened');
 }
 
-let editBottom = document.querySelector('.profile__edit-button');
-editBottom.addEventListener('click', openPopup);
+editBotton.addEventListener('click', openPopup);
+
+function closePopup() {
+  popup.classList.remove('popup__opened');
+}
+
+closeBotton.addEventListener('click', closePopup);
+
+let userName = document.querySelector('.popup__name');
+let userInfo = document.querySelector('.popup__info');
+
+userInfo.value;
+userInfo.value;
